558620e9c4a4b65f1c6b25d01dbe29f78dc01266
monophonik <70640207+monophonik@users.noreply.github.com>
2020-09-18 22:43:06 +0200

    Update Program.cs

--- Uppgift2/Villkor_och_loopar/Program.cs
+++ Uppgift2/Villkor_och_loopar/Program.cs
@@ -20,10 +20,28 @@ namespace Villkor_och_loopar
 
             int startNumberFast = 0;
 
-            int fastestTime = 0;
+            int fastestTime = int.MaxValue;
 
             int nrRunners = 0;
 
+            //int runTime = int.MaxValue;
+
+
+            int timeSec;
+
+            int startHr;
+            int startMin;
+            int startSec;
+            int startTimeSec;
+
+            int endHr;
+            int endMin;
+            int endSec;
+            int endTimeSec;
+
+            int bestEndHr;
+            int bestEndMin;
+            int bestEndSec;
 
             Console.Write("Ange ditt startnummer: ");
             startNumber = int.Parse(Console.ReadLine());
@@ -31,67 +49,65 @@ namespace Villkor_och_loopar
             while (startNumber > 1)
             {
 
-
-                //Anropa metod, deltagaren får mata in sina data.
-
-                fastestTime = RunTime();
-
-
-
-                nrRunners++;
-
-                Console.Write("Ange ditt startnummer: ");
-                startNumber = int.Parse(Console.ReadLine());
-            }
-
-            Console.WriteLine($"Antal tävlande i loppet: {nrRunners}");
-            Console.WriteLine($"Löparen med startnummer {startNumberFast} vann loppet.");
-            Console.WriteLine($"Den vinnande sluttiden är {fastestTime}");
-
-
-            //Methods
-
-            //Räknar ut lopptiden till sekunder
-            static int RunTime()
-            {
-                int time = 0;
-
-                int startHr;
-                int startMin;
-                int startSec;
-
-                int endHr;
-                int endMin;
-                int endSec;
                 
                 Console.Write("Vilken timme startade du: Ange 0-24: ");
-                startHr = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out startHr);
 
                 Console.Write("Vilken minut startade du? Ange 0-59: ");
-                startMin = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out startMin);
 
                 Console.Write("Vilken sekund startade du? Ange 0-59: ");
-                startSec = int.Parse(Console.ReadLine());
-
+                int.TryParse(Console.ReadLine(), out startSec);
 
                 Console.Write("Vilken timme gick du i mål? Ange 0-24: ");
-                endHr = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out endHr);
 
                 Console.Write("Vilken minut gick du i mål? Ange 0-59: ");
-                endMin = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out endMin);
 
                 Console.Write("Vilken sekund gick du i mål? Ange 0-59: ");
-                endSec = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out endSec);
 
-
+                startTimeSec = (startHr * 3600) + (startMin * 60) + startSec;
                 
-                //Passerat midnatt? 24h (86400) - startHr (i sekunder)
+                endTimeSec = (endHr * 3600) + (endMin * 60) + endSec;
+
+                timeSec = endTimeSec - startTimeSec;
+
+                //Kontroll för efter midnatt.
+                if (timeSec < 0)
+                {
+                    timeSec += 24 * 3600; //Ett dygn i sekunder för att undvika negativ sluttid.
+                }
 
                 //En timme = 3600 sek
                 //Ett dygn = 86400 sek
 
-                return time;
+                if (timeSec < fastestTime)
+                {
+
+                    startNumberFast = startNumber;
+                    fastestTime = timeSec;
+
+                }
+
+                nrRunners++;
+
+
+                Console.Write("Ange ditt startnummer: ");
+                int.TryParse(Console.ReadLine(), out startNumber);
             }
+
+
+            
+            bestEndHr = (int)Math.Floor((decimal)fastestTime / 3600);
+            bestEndMin = (int)Math.Floor((decimal)(fastestTime / 60)); //- endSec * 60);
+            bestEndSec = fastestTime % 60;
+
+            Console.WriteLine($"Antal tävlande i loppet: {nrRunners}");
+            Console.WriteLine($"Löparen med startnummer {startNumberFast} vann loppet.");
+            Console.WriteLine($"Den vinnande sluttiden är {bestEndHr} timmar, {bestEndMin} minuter, och {bestEndSec} sekunder.");
+                    
 
         }
 
